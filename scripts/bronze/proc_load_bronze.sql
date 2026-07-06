767e7d1a12fbd9c12af434627d052e1193573c3f
RitikaGangavati <127842013+RitikaGangavati@users.noreply.github.com>
2026-07-06 12:59:57 +0530

    Refactor bronze.load_bronze procedure for efficiency
    Refactored the procedure to remove duplicate print statements and streamline the loading process for CRM and ERP tables. Added error handling for loading failures.

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,93 +1,134 @@
+EXEC bronze.load_bronze;
+
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
+	BEGIN TRY
+			DECLARE @start_time DATETIME , @end_time DATETIME ,@batch_start_time DATETIME,@batch_end_time DATETIME ;
+			SET @batch_start_time= GETDATE();
+			PRINT('====================================');
+			PRINT('Loading data into bronze layer...');
+			PRINT('====================================');
 
-	PRINT('====================================');
-	PRINT('Loading data into bronze layer...');
-	PRINT('====================================');
+			PRINT('------------------------');
+			PRINT('Loading CRM Tables.');
+			PRINT('------------------------');
 
-	PRINT('------------------------');
-	PRINT('Loading CRM Tables.');
-	PRINT('------------------------');
+			SET @start_time= GETDATE();
+			PRINT('>> Truncating Table: bronze.crm_cust_info');
+			TRUNCATE TABLE bronze.crm_cust_info;
 
-	PRINT('>> Truncating Table: bronze.crm_cust_info');
-	TRUNCATE TABLE bronze.crm_cust_info;
-
-	PRINT('>> Loading data into Table: bronze.crm_cust_info');
-	BULK INSERT bronze.crm_cust_info
-	FROM 'C:\Users\ritug\Downloads\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-	WITH(
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	)
-
-
-	PRINT('>> Truncating Table: bronze.crm_prd_info');
-	TRUNCATE TABLE bronze.crm_prd_info;
-
-	PRINT('>> Loading data into Table: bronze.crm_prd_info');
-	BULK INSERT bronze.crm_prd_info
-	FROM 'C:\Users\ritug\Downloads\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-	WITH(
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	)
+			PRINT('>> Loading data into Table: bronze.crm_cust_info');
+			BULK INSERT bronze.crm_cust_info
+			FROM 'C:\Users\ritug\Downloads\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+			WITH(
+				FIRSTROW = 2,
+				FIELDTERMINATOR = ',',
+				TABLOCK
+			)
+			SET @end_time= GETDATE();
+			PRINT('>>> LOADING DURATION:'+ CAST(DATEDIFF(SECOND ,@start_time,@end_time) AS NVARCHAR)+'SECONDS');
+			PRINT('------------------------');
 
 
-	PRINT('>> Truncating Table: bronze.crm_sales_details');
-	TRUNCATE TABLE bronze.crm_sales_details;
+			SET @start_time= GETDATE();
+			PRINT('>> Truncating Table: bronze.crm_prd_info');
+			TRUNCATE TABLE bronze.crm_prd_info;
 
-	PRINT('>> Loading data into Table: bronze.crm_sales_details');
-	BULK INSERT bronze.crm_sales_details
-	FROM 'C:\Users\ritug\Downloads\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-	WITH(
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	)
-
-
-	PRINT('------------------------');
-	PRINT('Loading ERP Tables.');
-	PRINT('------------------------');
-
-	PRINT('>> Truncating Table: bronze.erp_cust_az12');
-	TRUNCATE TABLE bronze.erp_cust_az12;
-
-	PRINT('>> Loading data into Table: bronze.erp_cust_az12');
-	BULK INSERT bronze.erp_cust_az12
-	FROM 'C:\Users\ritug\Downloads\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
-	WITH (
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	)
+			PRINT('>> Loading data into Table: bronze.crm_prd_info');
+			BULK INSERT bronze.crm_prd_info
+			FROM 'C:\Users\ritug\Downloads\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+			WITH(
+				FIRSTROW = 2,
+				FIELDTERMINATOR = ',',
+				TABLOCK
+			)
+			SET @end_time= GETDATE();
+			PRINT('>>> LOADING DURATION:'+ CAST(DATEDIFF(SECOND ,@start_time,@end_time) AS NVARCHAR)+'SECONDS');
+			PRINT('------------------------');
 
 
-	PRINT('>> Truncating Table: bronze.erp_loc_a101');
-	TRUNCATE TABLE bronze.erp_loc_a101;
+			SET @start_time= GETDATE();
+			PRINT('>> Truncating Table: bronze.crm_sales_details');
+			TRUNCATE TABLE bronze.crm_sales_details;
 
-	PRINT('>> Loading data into Table: bronze.erp_loc_a101');
-	BULK INSERT bronze.erp_loc_a101
-	FROM 'C:\Users\ritug\Downloads\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
-	WITH(
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	)
+			PRINT('>> Loading data into Table: bronze.crm_sales_details');
+			BULK INSERT bronze.crm_sales_details
+			FROM 'C:\Users\ritug\Downloads\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+			WITH(
+				FIRSTROW = 2,
+				FIELDTERMINATOR = ',',
+				TABLOCK
+			)
+			SET @end_time= GETDATE();
+			PRINT('>>> LOADING DURATION:'+ CAST(DATEDIFF(SECOND ,@start_time,@end_time) AS NVARCHAR)+'SECONDS');
+			PRINT('------------------------');
 
 
-	PRINT('>> Truncating Table: bronze.erp_px_cat_g1v2');
-	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+			PRINT('------------------------');
+			PRINT('Loading ERP Tables.');
+			PRINT('------------------------');
 
-	PRINT('>> Loading data into Table: bronze.erp_px_cat_g1v2');
-	BULK INSERT bronze.erp_px_cat_g1v2
-	FROM 'C:\Users\ritug\Downloads\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
-	WITH(
-		FIRSTROW = 2,
-		FIELDTERMINATOR = ',',
-		TABLOCK
-	)
+			SET @start_time= GETDATE();
+			PRINT('>> Truncating Table: bronze.erp_cust_az12');
+			TRUNCATE TABLE bronze.erp_cust_az12;
 
+			PRINT('>> Loading data into Table: bronze.erp_cust_az12');
+			BULK INSERT bronze.erp_cust_az12
+			FROM 'C:\Users\ritug\Downloads\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+			WITH (
+				FIRSTROW = 2,
+				FIELDTERMINATOR = ',',
+				TABLOCK
+			)
+			SET @end_time= GETDATE();
+			PRINT('>>> LOADING DURATION:'+ CAST(DATEDIFF(SECOND ,@start_time,@end_time) AS NVARCHAR)+'SECONDS');
+			PRINT('------------------------');
+
+			SET @start_time= GETDATE();
+			PRINT('>> Truncating Table: bronze.erp_loc_a101');
+			TRUNCATE TABLE bronze.erp_loc_a101;
+
+			PRINT('>> Loading data into Table: bronze.erp_loc_a101');
+			BULK INSERT bronze.erp_loc_a101
+			FROM 'C:\Users\ritug\Downloads\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+			WITH(
+				FIRSTROW = 2,
+				FIELDTERMINATOR = ',',
+				TABLOCK
+			)
+			SET @end_time= GETDATE();
+			PRINT('>>> LOADING DURATION:'+ CAST(DATEDIFF(SECOND ,@start_time,@end_time) AS NVARCHAR)+'SECONDS');
+			PRINT('------------------------');
+
+			SET @start_time= GETDATE();
+			PRINT('>> Truncating Table: bronze.erp_px_cat_g1v2');
+			TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+			PRINT('>> Loading data into Table: bronze.erp_px_cat_g1v2');
+			BULK INSERT bronze.erp_px_cat_g1v2
+			FROM 'C:\Users\ritug\Downloads\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+			WITH(
+				FIRSTROW = 2,
+				FIELDTERMINATOR = ',',
+				TABLOCK
+			)
+			SET @end_time= GETDATE();
+			PRINT('>>> LOADING DURATION:'+ CAST(DATEDIFF(SECOND ,@start_time,@end_time) AS NVARCHAR)+'SECONDS');
+			PRINT('------------------------');
+
+			SET @batch_end_time= GETDATE();
+			PRINT('BATCH LOAD DURATION: '+ CAST(DATEDIFF(SECOND ,@batch_start_time,@batch_end_time) AS NVARCHAR)+ 'SECONDS');
+			PRINT('====================================');
+
+	END TRY
+
+	BEGIN CATCH
+			PRINT('====================================');
+			PRINT('ERROR OCCURED DURING LOADING BRONZE LAYER');
+			PRINT('ERROR MESSAGE:'+ CAST( ERROR_MESSAGE() AS NVARCHAR));
+			PRINT('ERROR MESSAGE:'+ CAST( ERROR_NUMBER() AS NVARCHAR));
+			PRINT('ERROR MESSAGE:'+ CAST( ERROR_STATE() AS NVARCHAR));
+
+			PRINT('====================================');
+	END CATCH
 END
